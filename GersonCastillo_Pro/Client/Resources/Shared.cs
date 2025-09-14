3a80b0c884011232ee11e90f1180e3ca746f8bfb
Gerson Castillo <gersoncl10000@outlook.com>
2025-09-14 21:00:53 +0200

    cambios en el diseño

--- GersonCastillo_Pro/Client/Resources/Shared.cs
+++
@@ -1,8 +0,0 @@
-// Esta clase permite que el IStringLocalizer<Shared> funcione correctamente
-// No necesita contener código, solo existe como referencia para los recursos
-namespace GersonCastillo_Pro.Client.Resources
-{
-    public class Shared
-    {
-    }
-}
